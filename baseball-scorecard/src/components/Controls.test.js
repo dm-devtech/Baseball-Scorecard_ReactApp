42fc8b6c53db22484fb664c658b53d379dff58be
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-02 16:56:50 +0000

    changed controls tests getByText to be case insensitive

--- baseball-scorecard/src/components/Controls.test.js
+++ baseball-scorecard/src/components/Controls.test.js
@@ -9,35 +9,35 @@ describe('<Controls />', () => {
   it('strike button when clicked goes to true - not mocked', () => {
     let clicked = false;
     const { getByText } = render(<Controls strike={() => clicked = true} />);
-    const strikeButton = getByText(/Strike/)
+    const strikeButton = getByText(/strike/i) // i - case insensitivity
     fireEvent.click(strikeButton)
     expect(clicked).toBe(true)
   });
   it('strike button mocked', () => {
     const click = jest.fn();
     const { getByText } = render(<Controls strike={click} />);
-    const strikeButton = getByText(/Strike/)
+    const strikeButton = getByText(/strike/i)
     fireEvent.click(strikeButton)
     expect(click).toBeCalled();
   });
   it('ball button mocked', () => {
     const click = jest.fn();
     const { getByText } = render(<Controls ball={click} />);
-    const ballButton = getByText(/Ball/)
+    const ballButton = getByText(/ball/i)
     fireEvent.click(ballButton)
     expect(click).toBeCalled();
   });
   it('foul button mocked', () => {
     const click = jest.fn();
     const { getByText } = render(<Controls foul={click} />);
-    const foulButton = getByText(/Foul/)
+    const foulButton = getByText(/foul/i)
     fireEvent.click(foulButton)
     expect(click).toBeCalled();
   });
   it('hit button mocked', () => {
     const click = jest.fn();
     const { getByText } = render(<Controls hit={click} />);
-    const hitButton = getByText(/Hit/)
+    const hitButton = getByText(/hit/i)
     fireEvent.click(hitButton)
     expect(click).toBeCalled();
   });
