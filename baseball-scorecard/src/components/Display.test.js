4cd3f2386520e5689dff9e557193d4bcd8ca679d
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-02 15:58:35 +0000

    added test to check page output

--- baseball-scorecard/src/components/Display.test.js
+++ baseball-scorecard/src/components/Display.test.js
@@ -4,6 +4,11 @@ import Display from './Display.js';
 
 describe('<Display />', () => {
   it('renders without crashing', () => {
-    render(<Display />)
+    render(<Display />);
+  });
+  it('checks props work', () => {
+    const display = render(<Display strikes={2} balls={3} />);
+    display.getByText(/Strikes: 2/)
+    display.getByText(/Balls: 3/)
   })
 })
