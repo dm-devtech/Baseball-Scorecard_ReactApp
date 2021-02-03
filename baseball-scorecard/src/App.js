84c74fa7e08df5a788126dbb4fba6fc709ec4d9b
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-03 17:02:04 +0000

    Adding state to app.js

--- baseball-scorecard/src/App.js
+++ baseball-scorecard/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './App.css';
 import Display from './components/Display.js'
 import Controls from './components/Controls.js'
 
 function App() {
+  const [strikes, setStrikes] = useState(0)
+  const [balls, setBalls] = useState(0)
   return (
     <div className="App">
       <Display strikes={2} balls={3} />
