2078af9e894fe3887915b7dbb8282ea8431f2485
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-03 17:48:13 +0000

    Splitting integration test into separate tests

--- baseball-scorecard/src/App.test.js
+++ baseball-scorecard/src/App.test.js
@@ -1,53 +1,79 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 import App from './App.js';
+
+afterEach(cleanup)
 
 describe('<Controls />', () => {
   it('renders without crashing', () => {
     render(<App />);
   });
-  it('full integration test', () => {
+  it('test strike works', () => {
+     // ^ = begning of block of text $ = end of line of text
+     const {getByText} = render(<App />);
+     const strike = getByText(/^strike$/i);
+     const ball = getByText(/^ball$/i);
+     const foul = getByText(/^foul$/i);
+     const hit = getByText(/^hit$/i);
+     // Test that strike works
+     fireEvent.click(strike);
+     fireEvent.click(strike);
+     getByText(/Strikes: 2/);
+  })
+  it('Test that ball works', () => {
     const {getByText} = render(<App />);
     const strike = getByText(/^strike$/i);
     const ball = getByText(/^ball$/i);
     const foul = getByText(/^foul$/i);
     const hit = getByText(/^hit$/i);
-     // ^ = begning of block of text $ = end of line of text
 
-     // Test that strike works
-     fireEvent.click(strike);
-     fireEvent.click(strike);
-     getByText(/Strikes: 2/);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Balls: 3/)
+    getByText(/Strikes: 0/);
+  })
+  it('Test that strike rolls over the count', () => {
+    const {getByText} = render(<App />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
 
-     // Test that ball works
-     fireEvent.click(ball);
-     fireEvent.click(ball);
-     fireEvent.click(ball);
-     getByText(/Balls: 3/)
+    fireEvent.click(strike);
+    fireEvent.click(strike);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    fireEvent.click(ball);
+    getByText(/Strikes: 0/)
+    getByText(/Balls: 0/)
+  })
+  it('Test that foul doesnt increase strikes past 2', () => {
+    const {getByText} = render(<App />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
 
-     // Test that strike rolls over the count
+     fireEvent.click(foul);
+     getByText(/Strikes: 1/)
+     fireEvent.click(foul);
+     getByText(/Strikes: 2/)
+     fireEvent.click(foul);
+     getByText(/Strikes: 2/)
+  })
+  it('check that hit resets count', () => {
+    const {getByText} = render(<App />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
+
      fireEvent.click(strike);
-     fireEvent.click(strike);
      fireEvent.click(ball);
-     fireEvent.click(ball);
-     fireEvent.click(ball);
-     fireEvent.click(ball);
+     fireEvent.click(hit);
      getByText(/Strikes: 0/)
-     getByText(/Balls: 0/)
-
-     // Test that foul doesnt increase strikes past 2
-      fireEvent.click(foul);
-      getByText(/Strikes: 1/)
-      fireEvent.click(foul);
-      getByText(/Strikes: 2/)
-      fireEvent.click(foul);
-      getByText(/Strikes: 2/)
-
-      // check that hit resets count
-      fireEvent.click(strike);
-      fireEvent.click(ball);
-      fireEvent.click(hit);
-      getByText(/Strikes: 0/)
-      getByText(/Strikes: 0/)
-  });
-});
+     getByText(/Strikes: 0/)
+  })
+})
