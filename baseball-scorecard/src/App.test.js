02f30d998654612cfc9cb2c190e9130f5d17421f
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-03 17:22:56 +0000

    Integration test App.js passes

--- baseball-scorecard/src/App.test.js
+++ baseball-scorecard/src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App from './App.js';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('<Controls />', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+  it('full integration test', () => {
+    const {getByText} = render(<App />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
+     // ^ = begning of block of text $ = end of line of text
+     // Test that strike works
+     fireEvent.click(strike);
+     fireEvent.click(strike);
+     getByText(/Strikes: 2/);
+  });
+});
