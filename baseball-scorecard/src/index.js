2b0f0b3bc4b08bd2914999190667e772a63a1411
dm-devtech <70208036+dm-devtech@users.noreply.github.com>
2021-02-08 18:09:59 +0000

    renamed css file

--- baseball-scorecard/src/index.js
+++ baseball-scorecard/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './CSS/_heading.css';
+import './CSS/formatting.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
