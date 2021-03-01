1366dc914a2d5c6b59e8acba8d523045e1ef606d
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-03-01 17:15:39 +0000

    added better buttons

--- baseball-scorecard/src/components/Controls.js
+++ baseball-scorecard/src/components/Controls.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Controls = (props) => {
   return (
     <div className="buttons">
-      <button onClick={props.strike}>Strike</button>
-      <button onClick={props.ball}>Ball</button>
-      <button onClick={props.foul}>Foul</button>
-      <button onClick={props.hit}>Hit</button>
+      <button className="click" onClick={props.strike}>Strike</button>
+      <button className="click" onClick={props.ball}>Ball</button>
+      <button className="click" onClick={props.foul}>Foul</button>
+      <button className="click" onClick={props.hit}>Hit</button>
     </div>
   )
 }
