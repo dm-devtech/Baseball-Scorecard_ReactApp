a6db25197e03d381b771cfe3da9bc2a1e2c7f9ce
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-07-28 12:30:18 +0100

    updated button colour

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Controls = (props) => {
   return (
     <div className="buttons">
-      <a type="button" class="btn btn-outline-dark" onClick={props.strike}>Strike</a>
-      <a type="button" class="btn btn-outline-dark" onClick={props.ball}>Ball</a>
-      <a type="button" class="btn btn-outline-dark" onClick={props.foul}>Foul</a>
-      <a type="button" class="btn btn-outline-dark" onClick={props.hit}>Hit</a>
+      <a type="button" class="btn btn-outline-light btn-lg" onClick={props.strike}>Strike</a>
+      <a type="button" class="btn btn-outline-light btn-lg" onClick={props.ball}>Ball</a>
+      <a type="button" class="btn btn-outline-light btn-lg" onClick={props.foul}>Foul</a>
+      <a type="button" class="btn btn-outline-light btn-lg" onClick={props.hit}>Hit</a>
     </div>
   )
 }
